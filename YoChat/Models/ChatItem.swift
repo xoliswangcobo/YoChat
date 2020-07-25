52a1c69532cb15bb1858c9134c3c692e4a15afeb
Xoliswa Ngcobo <xoliswangcobo@icloud.com>
2020-07-25 16:27:14 +0200

    Initial commit

---
+++ YoChat/Models/ChatItem.swift
@@ -0,0 +1,20 @@
+//
+//  ChatItem.swift
+//  YoChat
+//
+//  Created by Xoliswa on 2020/07/25.
+//  Copyright © 2020 Xoliswa. All rights reserved.
+//
+
+import Foundation
+
+enum ChatItemType: String, Codable {
+    case Image
+    case Text
+}
+
+class ChatItem: Codable {
+    var id: String
+    var type: ChatItemType
+    var data: String
+}
