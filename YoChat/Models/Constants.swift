49dbe7437f786d47a02d07cad9c1205553035800
Xoliswa Ngcobo <xoliswangcobo@icloud.com>
2020-07-26 00:21:32 +0200

    App contants

---
+++ YoChat/Models/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  YoChat
+//
+//  Created by Xoliswa on 2020/07/26.
+//  Copyright © 2020 Xoliswa. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+     static let dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+}
